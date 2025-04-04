d0b19d7a9bfba0c5e379a7af6f720acb3f9d7922
jcable <julian.cable@yahoo.com>
2025-04-04 13:13:10 +0100

    fix: refactor

---
+++ mailchimp_write/mailchimp.py
@@ -0,0 +1,35 @@
+import json
+import os
+import boto3
+from sync import get_client, getlistid, crud, archive
+
+ssm = boto3.client('ssm')
+
+def update_mailchimp(action, record):
+  # print('update_mailchimp', action, record)
+  excludes = {}
+  if 'EXCLUDE' in os.environ:
+    excludes = json.loads(os.environ.get('EXCLUDE'))
+  r = ssm.get_parameter(Name='/MAILCHIMP/API_KEY', WithDecryption=True)
+  apiKey = r['Parameter']['Value']
+  r = ssm.get_parameter(Name='/MAILCHIMP/SERVER')
+  server = r['Parameter']['Value']
+  r = ssm.get_parameter(Name='/MAILCHIMP/AUDIENCE')
+  audience = r['Parameter']['Value']
+  client = get_client()
+  client.set_config({
+   "api_key": apiKey,
+   "server": server,
+  })
+  list = getlistid(client, audience)
+  if action == 'added':
+    # print('add', record)
+    crud(client, list, record)
+  elif action == 'changed':
+    # print('change', record)
+    crud(client, list, record['after'])
+  elif action == 'deleted':
+    # print('delete', record)
+    archive(client, list, record)
+  else:
+    print(action, json.dumps(record))
